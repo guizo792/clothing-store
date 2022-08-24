d3e27f7a726e714bb6b767a22d5c7901470750db
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-08-24 12:39:30 +0100

    created addToCart feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,6 +8,7 @@ import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => {
     setIsCartOpen(!isCartOpen);
@@ -16,7 +17,7 @@ const CartIcon = () => {
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
