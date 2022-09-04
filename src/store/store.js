12fe92fd78d592f895ca9915a5ceb4e33b797336
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-09-04 23:52:36 +0100

    added redux persistence

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,29 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
+// import { loggerMiddleware } from 'redux-logger';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) return next(action);
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-  console.log('next state: ', store.getState());
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
 };
 
-const middlewares = [loggerMiddleware];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
