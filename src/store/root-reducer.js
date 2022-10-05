47d2fb994a7ec59ad4166de19c894311d637a4c4
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-10-05 15:58:40 +0100

    typed cart selector

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 import { cartReducer } from './cart/cart.reducer';
-import { categoriesReducer } from './categories/category.reducer.ts';
+import { categoriesReducer } from './categories/category.reducer';
 import { userReducer } from './user/user.reducer';
 
 export const rootReducer = combineReducers({
