b95d5943c61d9ce861acc852fc46f1a7a54c3c0e
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-10-06 23:50:39 +0100

    typed out store and completed selectors typing

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { compose, createStore, applyMiddleware, Middleware } from 'redux';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 // import thunk from 'redux-thunk';
@@ -9,10 +9,22 @@ import { rootSaga } from './root-saga';
 import { rootReducer } from './root-reducer';
 // import { loggerMiddleware } from 'redux-logger';
 
-const persistConfig = {
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
-  whiteList: ['cart'],
+  whitelist: ['cart'],
 };
 
 const sagaMiddleware = createSagaMiddleware();
@@ -22,7 +34,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middlewares = [
   process.env.NODE_ENV !== 'production' && logger,
   sagaMiddleware,
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
