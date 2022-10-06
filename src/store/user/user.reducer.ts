dbde2b66d697862313dd478ae790a5cd9ae6b9cb
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-10-06 23:27:48 +0100

    typed out user/ action&reducer&selector&types files

---
+++ src/store/user/user.reducer.ts
@@ -0,0 +1,38 @@
+import { AnyAction } from 'redux';
+import { UserData } from '../../utils/firebase/firebase.utils';
+import { USER_ACTION_TYPES } from './user.types';
+import {
+  signInFailed,
+  signUpFailed,
+  signOutFailed,
+  signInSuccess,
+  signOutSuccess,
+} from './user.action';
+
+export type UserState = {
+  readonly currentUser: UserData | null;
+  readonly isLoading: boolean;
+  error: Error | null;
+};
+
+const INITIAL_STATE: UserState = {
+  currentUser: null,
+  isLoading: false,
+  error: null,
+};
+
+export const userReducer = (state = INITIAL_STATE, action: AnyAction) => {
+  if (signInSuccess.match(action)) {
+    return { ...state, currentUser: action.payload };
+  }
+
+  if (
+    signInFailed.match(action) ||
+    signUpFailed.match(action) ||
+    signOutFailed.match(action)
+  ) {
+    return { ...state, error: action.payload };
+  }
+
+  return state;
+};
