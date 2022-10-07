4edb6b5dcca066a3c6b17f7bd1667eb316414d49
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-10-07 18:29:00 +0100

    typed categories saga

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'typed-redux-saga';
 
 import { getCategoriesAndDocs } from '../../utils/firebase/firebase.utils';
 import {
@@ -9,20 +9,20 @@ import { CATEGORIES_ACTION_TYPES } from './category.types';
 
 export function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocs, 'categories');
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(getCategoriesAndDocs);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailure(error));
+    yield* put(fetchCategoriesFailure(error as Error));
   }
 }
 
 export function* onFetchCategories() {
-  yield takeLatest(
+  yield* takeLatest(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
     fetchCategoriesAsync
   );
 }
 
 export function* categoriesSaga() {
-  yield all([call(onFetchCategories)]);
+  yield* all([call(onFetchCategories)]);
 }
